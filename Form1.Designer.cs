de64360620a80d879bf38dbc1fd0e6edb69a27a0
DragonNP <dragonnp@ya.ru>
2019-07-13 14:59:54 +0300

    Перенос проекта
    gpgsig -----BEGIN PGP SIGNATURE-----
     
     wsBcBAABCAAQBQJdKbilCRBK7hj4Ov3rIwAAdHIIAIRllhCgq8YWJ0d/EhfI3k0i
     yLvPzucprtQSKJmmIEhVt5DYUfaQfd7HKp6hxiwCer/dDyjQY/yMI0jR8kstv/it
     iRYhQEjR2qzv86P5DINAebkkAmrZZO8UB1cOHNacTkxTSV9zLZ3rOowADHrpWFIW
     VoYSMkWMeRIvAiNcvAAkm2r7Sc2g1hscG1T02p7gef8XBVT5TviVW2Oj1MIWn/Cb
     bimZAV4Dizl6AGqrShtS7s2icepgeH0F4czlORe+WdMK4QKgTVPJssuou1oTFRZz
     aX30bX41f8dlGDPrZ+aU2uouQyylAB6fvp4gxHkq2Y6t827GUFbf4PNi2PyHonQ=
     =S8g9
     -----END PGP SIGNATURE-----
     
    
    Дополнение readme.md

---
+++ Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace EasyGraph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Done = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.showValue = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.name = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            this.chart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chart.BorderSkin.SkinStyle = System.Windows.Forms.DataVisualization.Charting.BorderSkinStyle.Emboss;
+            this.chart.Location = new System.Drawing.Point(13, 193);
+            this.chart.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.chart.Name = "chart";
+            this.chart.Size = new System.Drawing.Size(789, 409);
+            this.chart.TabIndex = 5;
+            this.chart.TextAntiAliasingQuality = System.Windows.Forms.DataVisualization.Charting.TextAntiAliasingQuality.SystemDefault;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI Semilight", 18F);
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.richTextBox1.Location = new System.Drawing.Point(60, 43);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.richTextBox1.Multiline = false;
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(379, 32);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // Done
+            // 
+            this.Done.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Done.FlatAppearance.BorderSize = 0;
+            this.Done.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Done.Font = new System.Drawing.Font("Segoe UI Semilight", 18F);
+            this.Done.Location = new System.Drawing.Point(166, 151);
+            this.Done.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.Done.Name = "Done";
+            this.Done.Size = new System.Drawing.Size(137, 38);
+            this.Done.TabIndex = 8;
+            this.Done.Text = "Построить";
+            this.Done.UseVisualStyleBackColor = false;
+            this.Done.Click += new System.EventHandler(this.Done_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semilight", 18F);
+            this.label1.Location = new System.Drawing.Point(27, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 32);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "x=";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semilight", 18F);
+            this.label2.Location = new System.Drawing.Point(27, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 32);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "y=";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Font = new System.Drawing.Font("Segoe UI Semilight", 18F);
+            this.richTextBox2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.richTextBox2.Location = new System.Drawing.Point(60, 79);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.richTextBox2.Multiline = false;
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(379, 32);
+            this.richTextBox2.TabIndex = 10;
+            this.richTextBox2.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showValue});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(814, 24);
+            this.menuStrip1.TabIndex = 15;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // showValue
+            // 
+            this.showValue.Name = "showValue";
+            this.showValue.Size = new System.Drawing.Size(117, 20);
+            this.showValue.Text = "Показать значения";
+            this.showValue.Click += new System.EventHandler(this.ShowValue_Click);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox3.Font = new System.Drawing.Font("Segoe UI Semilight", 16F);
+            this.richTextBox3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.richTextBox3.Location = new System.Drawing.Point(446, 44);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(356, 144);
+            this.richTextBox3.TabIndex = 16;
+            this.richTextBox3.Text = "";
+            // 
+            // name
+            // 
+            this.name.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.name.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.name.Font = new System.Drawing.Font("Segoe UI Semilight", 18F);
+            this.name.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.name.Location = new System.Drawing.Point(76, 115);
+            this.name.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.name.Multiline = false;
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(308, 32);
+            this.name.TabIndex = 17;
+            this.name.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(814, 613);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Done);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "f";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button Done;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showValue;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox name;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace EasyGraph
+{
+    public partial class Form1 : Form
+    {
+        private bool isContinue = true;
+        private List<double> x = new List<double>();
+        private List<double> y = new List<double>();
+        private readonly Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart.Initialize(title: "График", legendsTitle: "Легенда");
+        }
+
+        private void ShowValue_Click(object sender, EventArgs e)
+        {
+            richTextBox3.Text = $"x = {string.Join("; ", x)}\n" +
+                $"y = { string.Join("; ", y)}";
+        }
+
+        private List<double> Random(string text)
+        {
+
+            double min, max, col;
+            text = text.Replace("random", "");
+            text = text.Replace("(", "");
+            text = text.Replace(")", "");
+            text = text.Replace(" ", "");
+            string[] str = text.Split(',');
+            List<double> list = new List<double>();
+
+            min = double.Parse(str[0], System.Globalization.CultureInfo.InvariantCulture);
+            max = double.Parse(str[1], System.Globalization.CultureInfo.InvariantCulture);
+            col = double.Parse(str[2], System.Globalization.CultureInfo.InvariantCulture);
+
+            for (int i = 0; i <= col; i++)
+                list.Add(Math.Round(random.NextDouble() * (max - min) + min, 2));
+            return list;
+        }
+
+        private List<double> Parser(string text)
+        {
+            double first, step, end;
+            string[] str = text.Split(':');
+            List<double> list = new List<double>();
+
+            if (str.Length == 3)
+            {
+                first = double.Parse(str[0], System.Globalization.CultureInfo.InvariantCulture);
+                step = double.Parse(str[1], System.Globalization.CultureInfo.InvariantCulture);
+                end = double.Parse(str[2], System.Globalization.CultureInfo.InvariantCulture);
+
+                for (double i = first; i <= end; i += step)
+                    list.Add(i);
+                return list;
+            }
+            else
+            {
+                MessageBox.Show(caption: "Ошибка",
+                    text: "Неверно задан пареметр!\nФормат нач.значение:шаг:кон.значение или нач.значение:кон.значение",
+                    buttons: MessageBoxButtons.OK,
+                    icon: MessageBoxIcon.Error);
+                isContinue = false;
+                return null;
+            }
+        }
+
+        private void Done_Click(object sender, EventArgs e)
+        {
+            x.Clear();
+            y.Clear();
+
+            Invoke((MethodInvoker)(() =>
+            {
+                isContinue = true;
+                if (richTextBox1.Text.Contains(":"))
+                    x = Parser(richTextBox1.Text);
+                else if (richTextBox1.Text == "y")
+                    x = y;
+                else if (richTextBox1.Text.Contains("random"))
+                    x = Random(richTextBox1.Text);
+                else
+                {
+                    foreach (string i in richTextBox1.Text.Split(new char[] { '\n', ' ' }))
+                        x.Add(double.Parse(i, System.Globalization.CultureInfo.InvariantCulture));
+                }
+            }));
+
+            Invoke((MethodInvoker)(() =>
+            {
+                isContinue = true;
+                if (richTextBox2.Text.Contains(":"))
+                    y = Parser(richTextBox2.Text);
+                else if (richTextBox2.Text == "x")
+                    y = x;
+                else if (richTextBox2.Text.Contains("random"))
+                    y = Random(richTextBox2.Text);
+                else
+                {
+                    foreach (string i in richTextBox2.Text.Split(new char[] { '\n', ' ' }))
+                        y.Add(double.Parse(i, System.Globalization.CultureInfo.InvariantCulture));
+                }
+            }));
+
+            if (isContinue)
+                ToDisplayGraph(x, y);
+        }
+
+        private void ToDisplayGraph(List<double> x, List<double> y)
+        {
+            double minX = -1, maxX = -1;
+            double minY = -1, maxY = -1;
+
+            Invoke((MethodInvoker)(() =>
+            {
+                bool first = true;
+                for (int i = 0; i < (x.Count >= y.Count ? y.Count : x.Count); i++)
+                {
+                    if (first)
+                    {
+                        first = false;
+                        minY = y[i];
+                        maxY = y[i];
+
+                        minX = x[i];
+                        maxX = x[i];
+                    }
+
+                    if (minY > y[i]) minY = y[i];
+                    else if (maxY < y[i]) maxY = y[i];
+
+                    if (minX > x[i]) minX = x[i];
+                    else if (maxX < x[i]) maxX = x[i];
+                }
+
+                //chart.Series.Clear();
+                chart.ChartAreas["area"].AxisX.Minimum = minX;
+                chart.ChartAreas["area"].AxisX.Maximum = maxX;
+
+                chart.ChartAreas["area"].AxisY.Minimum = minY;
+                chart.ChartAreas["area"].AxisY.Maximum = maxY;
+
+                chart.Series.Add(name.Text);
+                chart.Series[name.Text].BorderDashStyle = ChartDashStyle.Solid;
+                chart.Series[name.Text].BorderWidth = 5;
+                chart.Series[name.Text].Color = Color.DarkBlue;
+                chart.Series[name.Text].Font = new Font("Segoe UI Semilight", 18, FontStyle.Regular);
+                chart.Series[name.Text].ChartType = SeriesChartType.Line;
+                chart.Update();
+
+                for (int i = 0; i < (x.Count >= y.Count ? y.Count : x.Count); i++)
+                {
+                    chart.Series[name.Text].Points.AddXY(x[i], y[i]);
+                    chart.Update();
+                }
+            }));
+        }
+    }
+    public static class Graph
+    {
+        public static void Initialize(this Chart chart,
+                                      string title = "График by DragonNP",
+                                      string areasName = "area",
+                                      string legendsTitle = "Legends",
+                                      string axisXTitle = "X",
+                                      string axisYTitle = "Y",
+                                      AxisArrowStyle axisArrowStyle = AxisArrowStyle.Triangle,
+                                      Font font = null)
+        {
+            if (font == null) font = new Font("Segoe UI Semilight", 18, FontStyle.Regular);
+
+            chart.Titles.Add(title).Font = font;
+            chart.Legends.Add(title);
+            chart.Legends[title].Title = legendsTitle;
+
+            chart.ChartAreas.Add(areasName);
+            chart.ChartAreas[areasName].AxisX.Title = axisXTitle;
+            chart.ChartAreas[areasName].AxisX.ArrowStyle = axisArrowStyle;
+            chart.ChartAreas[areasName].AxisX.TitleFont = font;
+            chart.ChartAreas[areasName].AxisX.TitleForeColor = Color.Black;
+
+            chart.ChartAreas[areasName].AxisY.Title = axisYTitle;
+            chart.ChartAreas[areasName].AxisY.ArrowStyle = axisArrowStyle;
+            chart.ChartAreas[areasName].AxisY.TitleFont = font;
+            chart.ChartAreas[areasName].AxisY.TitleForeColor = Color.Black;
+        }
+    }
+}
